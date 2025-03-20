259fff67ac372b549f197ba1f3d0ae20e3a9459f
Ananda Prema <77527794+menandap@users.noreply.github.com>
2025-03-20 14:30:00 +0800

    Update main.js

--- Collecting Star/src/main.js
+++ Collecting Star/src/main.js
@@ -1,18 +1,18 @@
 import Phaser from 'phaser'
 
-import CoronaBusterScene from './scenes/CoronaBusterScene'
+import CollectingStarsScene from './scenes/CollectingStarsScene'
 
 const config = {
 	type: Phaser.AUTO,
-	width: 400,
-	height: 620,
+	width: 800,
+	height: 600,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 }
 		}
 	},
-	scene: [CoronaBusterScene]
+	scene: [CollectingStarsScene]
 }
 
 export default new Phaser.Game(config)
